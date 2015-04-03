c8c8252a20f1dcf1ab79e196904cf712823679e3
Darin Hoover <darinh@gmail.com>
2015-04-02 17:48:34 -0700

    Organized project structure and built dashboard view stub.

---
+++ app/www/app/dashboard/dashboard.js
@@ -0,0 +1,28 @@
+angular.module('hydra.dashboard', [
+
+])
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('hydra.dashboard', {
+                url: "/dashboard",
+                views: {
+                    'menuContent': {
+                        templateUrl: "app/dashboard/dashboard.tmpl.html",
+                        controller: 'DashboardCtrl as dashboardCtrl'
+                    }
+                }
+            })
+    })
+
+    .controller('DashboardCtrl', function() {
+        var dashboardCtrl = this;
+        dashboardCtrl.playlists = [
+            { title: 'Test', id: 1 },
+            { title: 'Chill', id: 2 },
+            { title: 'Dubstep', id: 3 },
+            { title: 'Indie', id: 4 },
+            { title: 'Rap', id: 5 },
+            { title: 'Cowbell', id: 6 }
+        ];
+    })
+;
