9b5f9b275cf467333e17383bd3d1262bd0ec5afb
Darin Hoover <darinh@gmail.com>
2015-04-05 13:04:14 -0700

    Finished transmitter list view with static data.

--- app/www/app/transmitters/transmitter/transmitter.js
+++ app/www/app/transmitters/transmitter/transmitter.js
@@ -1,16 +1,24 @@
 angular.module('hydra.transmitters.transmitter', [])
     .config(function($stateProvider) {
         $stateProvider
-            .state('hydra.transmitters.transmitter', {
+            .state('hydra.transmitter', {
                 url: "/transmitters/transmitter/:transmitterId",
                 views: {
                     'menuContent': {
                         templateUrl: "app/transmitters/transmitter/transmitter.tmpl.html",
-                        controller: 'TransmitterCtrl as TransmitterCtrl'
+                        controller: 'TransmitterCtrl as transmitterCtrl'
                     }
                 }
             })
     })
-    .controller('TransmitterCtrl', function(){
+    .controller('TransmitterCtrl', function($stateParams, $state){
         var transmitterCtrl = this;
+
+        // if no transmitter id provided, change state to hydra.transmitters
+        if ($stateParams.transmitterId == '')
+        {
+            $state.go('hydra.transmitters')
+        }
+
+        transmitterCtrl.transmitterId = $stateParams.transmitterId;
     });
