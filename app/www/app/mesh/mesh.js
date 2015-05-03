3e062317f2a22fa0a756fd316c40ca6ff5e4578e
Darin Hoover <darinh@gmail.com>
2015-05-02 22:16:05 -0700

    Changed typeahead to dropdown list, since keyboard covers suggestions.

--- app/www/app/mesh/mesh.js
+++ app/www/app/mesh/mesh.js
@@ -20,6 +20,8 @@ angular.module('hydra.mesh', [
         var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
         
         meshCtrl.states = states;
+        
+        meshCtrl.typeAheadTemplateUrl = "app/mesh/typeAhead.tmpl.html";
 
         var transmitter = {
             name: 'Transmitter ' + $stateParams.transmitterId,
