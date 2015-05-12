38e8a97b1d22b71b6244b09d3a77d47f5b26d418
Kevin Mershon <kevin@slixbits.com>
2015-05-12 12:10:47 -0700

    Implement getting the neighbors for each transmitter.

--- app/www/app/services/transmitterService.js
+++ app/www/app/services/transmitterService.js
@@ -1,52 +1,65 @@
 hero.service('TransmitterService', function() {
-		var inProduction = (window.hart && window.parser);
+        var inProduction = (window.hart && window.parser);
 
-		/** @function
-		 * @name getAll
-		 * Retrieves an array of Transmitters
-		 */
-		var getAll = function() {
-			var transmitters;
+        /** @function
+         * @name getAll
+         * Retrieves an array of Transmitters
+         */
+        var getAll = function() {
+            var transmitters;
 
-			if (inProduction)
-			{
-				// retrieve collection of transmitters from the parser.
-				// TODO: Add neighbor data for mesh view.
-				transmitters =  _.map(
-		            window.parser.getTransmitterList(),
-		            function(t) {
-		                return {
-		                    connected: t['status'] === 208,
-		                    name: t['name'],
-		                    mac: t['macAddress']
-		                };
-		            }
-				);
+            if (inProduction)
+            {
+                // retrieve collection of transmitters from the parser.
+                transmitters =  _.map(
+                    window.parser.getTransmitterList(),
+                    function(t) {
+                        var stats = window.parser.neighborStatistics[t['macAddress']];
+                        var neighbors;
+                        if (stats === undefined) {
+                            neighbors = [];
+                        } else {
+                            neighbors = _.map(
+                                stats.neighbors,
+                                function(n) {
+                                    return n['macAddress'];
+                                }
+                            );
+                        }
 
-
-			}
-			else
-			{
-				// not in production -- return dummy data
-				transmitters = [
-					{ connected: true, name: "Transmitter 1", mac: "01-02-03-04-05-06-07-08", neighbors: ["02-03-04-05-06-07-08-09","03-04-05-06-07-08-09-10","04-05-06-07-08-09-10-11"] },
-					{ connected: true, name: "Transmitter 2", mac: "02-03-04-05-06-07-08-09", neighbors: ["01-02-03-04-05-06-07-08","05-06-07-08-09-10-11-12"] },
-					{ connected: true, name: "Transmitter 3", mac: "03-04-05-06-07-08-09-10", neighbors: ["01-02-03-04-05-06-07-08"] },
-					{ connected: true, name: "Transmitter 4", mac: "04-05-06-07-08-09-10-11", neighbors: ["01-02-03-04-05-06-07-08","05-06-07-08-09-10-11-12","06-07-08-09-10-11-12-13"] },
-					{ connected: true, name: "Transmitter 5", mac: "05-06-07-08-09-10-11-12", neighbors: ["04-05-06-07-08-09-10-11","02-03-04-05-06-07-08-09","06-07-08-09-10-11-12-13","07-08-09-10-11-12-13-14"] },
-					{ connected: true, name: "Transmitter 6", mac: "06-07-08-09-10-11-12-13", neighbors: ["04-05-06-07-08-09-10-11","05-06-07-08-09-10-11-12","07-08-09-10-11-12-13-14"] },
-					{ connected: true, name: "Transmitter 7", mac: "07-08-09-10-11-12-13-14", neighbors: ["05-06-07-08-09-10-11-12","06-07-08-09-10-11-12-13","08-09-10-11-12-13-14-15"] },
-					{ connected: true, name: "Transmitter 8", mac: "08-09-10-11-12-13-14-15", neighbors: ["07-08-09-10-11-12-13-14"] }
-
-				];
-			}
-
-			return transmitters;
-		};
+                        return {
+                            connected: t['status'] === 208,
+                            name: t['name'],
+                            mac: t['macAddress'],
+                            neighbors: neighbors
+                        };
+                    }
+                );
 
 
-		var fetchByMacAddress = function(macAddress) {
-			return _.reduce(
+            }
+            else
+            {
+                // not in production -- return dummy data
+                transmitters = [
+                    { connected: true, name: "Transmitter 1", mac: "01-02-03-04-05-06-07-08", neighbors: ["02-03-04-05-06-07-08-09","03-04-05-06-07-08-09-10","04-05-06-07-08-09-10-11"] },
+                    { connected: true, name: "Transmitter 2", mac: "02-03-04-05-06-07-08-09", neighbors: ["01-02-03-04-05-06-07-08","05-06-07-08-09-10-11-12"] },
+                    { connected: true, name: "Transmitter 3", mac: "03-04-05-06-07-08-09-10", neighbors: ["01-02-03-04-05-06-07-08"] },
+                    { connected: true, name: "Transmitter 4", mac: "04-05-06-07-08-09-10-11", neighbors: ["01-02-03-04-05-06-07-08","05-06-07-08-09-10-11-12","06-07-08-09-10-11-12-13"] },
+                    { connected: true, name: "Transmitter 5", mac: "05-06-07-08-09-10-11-12", neighbors: ["04-05-06-07-08-09-10-11","02-03-04-05-06-07-08-09","06-07-08-09-10-11-12-13","07-08-09-10-11-12-13-14"] },
+                    { connected: true, name: "Transmitter 6", mac: "06-07-08-09-10-11-12-13", neighbors: ["04-05-06-07-08-09-10-11","05-06-07-08-09-10-11-12","07-08-09-10-11-12-13-14"] },
+                    { connected: true, name: "Transmitter 7", mac: "07-08-09-10-11-12-13-14", neighbors: ["05-06-07-08-09-10-11-12","06-07-08-09-10-11-12-13","08-09-10-11-12-13-14-15"] },
+                    { connected: true, name: "Transmitter 8", mac: "08-09-10-11-12-13-14-15", neighbors: ["07-08-09-10-11-12-13-14"] }
+
+                ];
+            }
+
+            return transmitters;
+        };
+
+
+        var fetchByMacAddress = function(macAddress) {
+            return _.reduce(
                 this.getAll(),
                 function(ignore, t) {
                   if (t['macAddress'] == macAddress) {
@@ -58,8 +71,8 @@ hero.service('TransmitterService', function() {
                   }
                 }
             );
-		}
+        }
 
-		this.getAll = getAll;
-		this.fetchByMacAddress = fetchByMacAddress;
-	});
+        this.getAll = getAll;
+        this.fetchByMacAddress = fetchByMacAddress;
+    });
