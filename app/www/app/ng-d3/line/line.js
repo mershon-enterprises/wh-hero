50523c3935de3f3f050d90a782993255ecd562c8
Kevin Mershon <kevin@slixbits.com>
2015-05-12 11:38:18 -0700

    Formatting

--- app/www/app/ng-d3/line/line.js
+++ app/www/app/ng-d3/line/line.js
@@ -14,35 +14,34 @@ angular.module('ng-d3.line', [])
                 var width = $window.innerWidth,
                     height = $window.innerHeight * 0.5;
 
-
-                    var chart = c3.generate({
-                        bindto: elem[0],
-                        data: {
-                            json: scope.data,
-                            keys: {
-                                x: 'date',
-                                value: ['value']
-                            },
-                            xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
-                            type: 'spline'
+                var chart = c3.generate({
+                    bindto: elem[0],
+                    data: {
+                        json: scope.data,
+                        keys: {
+                            x: 'date',
+                            value: ['value']
                         },
-                        axis: {
-                            x: {
-                                type: 'timeseries',
-                                tick: {
-                                    format: '%H:%M:%S',
-                                    culling: {
-                                        max: 5
-                                    }
+                        xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
+                        type: 'spline'
+                    },
+                    axis: {
+                        x: {
+                            type: 'timeseries',
+                            tick: {
+                                format: '%H:%M:%S',
+                                culling: {
+                                    max: 5
                                 }
-                            },
-                            y: {
-                               tick: {
-                                   format: d3.format('.2f')
-                               }
+                            }
+                        },
+                        y: {
+                           tick: {
+                               format: d3.format('.2f')
                            }
-                        }
-                    });
+                       }
+                    }
+                });
             }
         }
     });
