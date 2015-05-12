e3482d8ff86384ef599838c4b8dc57bdcdd8123e
Kevin Mershon <kevin@slixbits.com>
2015-05-11 19:40:19 -0700

    Finish implementing neighbor statistics for all transmitters with retry

--- plugins/wireless-hart-gateway/www/parser.js
+++ plugins/wireless-hart-gateway/www/parser.js
@@ -56,6 +56,30 @@ module.exports = {
     return deferred.promise;
   },
 
+  maybeGetTransmitter: function(deviceIndex) {
+    var self = this;
+    var deferred = window.$q.defer();
+
+    if (self.transmitters[deviceIndex] === undefined) {
+      console.log('getting transmitter ' + deviceIndex);
+      window.hart.getTransmitter(self.gateway, deviceIndex).then(
+        function(transmitter) {
+          self.transmitters[deviceIndex] = transmitter;
+
+          deferred.resolve(transmitter);
+        },
+        function(message) {
+          console.log('Failed to get transmitter ' + deviceIndex);
+          deferred.reject();
+        }
+      );
+    } else {
+      deferred.resolve(self.transmitters[deviceIndex]);
+    }
+
+    return deferred.promise;
+  },
+
   poll: function(deviceIndex) {
     var self = this;
     if (self.pollingEnabled === false) {
@@ -65,33 +89,7 @@ module.exports = {
 
     console.log('polling for device ' + deviceIndex);
 
-    // if we don't have meta-data on the transmitter, get that first
-    var transmitter = self.transmitters[deviceIndex];
-
-    var maybeGetTransmitter = function() {
-      var deferred = window.$q.defer();
-
-      if (self.transmitters[deviceIndex] === undefined) {
-        console.log('getting transmitter ' + deviceIndex);
-        window.hart.getTransmitter(self.gateway, deviceIndex).then(
-          function(transmitter) {
-            self.transmitters[deviceIndex] = transmitter;
-            deferred.resolve(transmitter);
-          },
-          function(message) {
-            console.log('Failed to get transmitter ' + deviceIndex);
-            deferred.reject();
-          }
-        );
-      } else {
-        deferred.resolve(self.transmitters[deviceIndex]);
-      }
-
-      return deferred.promise;
-    };
-
-    // but normally we can just get the hart variables
-    maybeGetTransmitter().then(
+    self.maybeGetTransmitter(deviceIndex).then(
       function(transmitter) {
         console.log('getting hart variables for transmitter transmitter ' +
                     deviceIndex);
@@ -104,38 +102,6 @@ module.exports = {
       function(hartVariables) {
         self.hartVariables[self.transmitters[deviceIndex]['macAddress']] = hartVariables;
 
-        // after getting the hart variables, get the neighbor info
-        console.log('getting neighbor statistics for transmitter transmitter ' +
-                    deviceIndex);
-        return window.hart.getNeighborStatistics(self.gateway,
-            self.transmitters[deviceIndex]);
-      },
-      function(message) {
-        console.log('Failed to get hart variables for transmitter ' +
-                    deviceIndex);
-      }
-    ).then(
-      function(hartMessage) {
-        // if we get status 33 or 34, keep trying once per second until we
-        // get neighbor stats
-        //
-        //var tryStatistics = function(xmtr) {
-        if (hartMessage.status === 33 || hartMessage.status === 34) {
-          if (self.pollingEnabled === true) {
-            // FIXME -- implement
-            console.log('retry statistics for transmitter ' + deviceIndex);
-            // window.setTimeout(
-            //   function() {
-            //     tryStatistics(xmtr);
-            //   },
-            //   1000
-            // );
-          }
-        } else {
-          self.neighborStatistics[self.transmitters[deviceIndex]['macAddress']] = hartMessage;
-        }
-        // tryStatistics(transmitter);
-
         // now, poll the next transmitter
         if (self.pollingEnabled === true) {
           window.setTimeout(
@@ -147,6 +113,64 @@ module.exports = {
               }
             }, 50 // ms
           );
+        }
+      },
+      function(message) {
+        console.log('Failed to get hart variables for transmitter ' +
+                    deviceIndex);
+      }
+    );
+  },
+
+  pollStatistics: function(deviceIndex) {
+    var self = this;
+    if (self.pollingEnabled === false) {
+      console.log('not polling');
+      return;
+    }
+
+    // after getting the hart variables, get the neighbor info
+    console.log('getting neighbor statistics for transmitter transmitter ' +
+                deviceIndex);
+
+    self.maybeGetTransmitter(deviceIndex).then(
+      function(transmitter) {
+        return window.hart.getNeighborStatistics(self.gateway, transmitter);
+      },
+      function() {
+        // shouldn't happen???
+      }
+    ).then(
+      function(hartMessage) {
+        // if we get status 33 or 34, keep trying once per second until we
+        // get neighbor stats
+        //
+        if (hartMessage.status === 33 || hartMessage.status === 34) {
+          if (self.pollingEnabled === true) {
+            console.log('retry statistics for transmitter ' + deviceIndex);
+            window.setTimeout(
+              function() {
+                self.pollStatistics(deviceIndex);
+              },
+              1000
+            );
+          }
+        } else {
+          self.neighborStatistics[self.transmitters[deviceIndex]['macAddress']] = hartMessage;
+
+          // now, poll the next transmitter
+          if (self.pollingEnabled === true) {
+            window.setTimeout(
+              function() {
+                if (deviceIndex === self.deviceCount) {
+                  self.pollStatistics(1); // start over at 1
+                } else {
+                  self.pollStatistics(deviceIndex + 1);
+                }
+              }, 50 // ms
+            );
+          }
+
         }
       },
       function() {
@@ -163,6 +187,7 @@ module.exports = {
     this.pollingEnabled = true;
     console.log('Polling enabled');
     this.poll(1);
+    this.pollStatistics(1);
   },
 
   disablePolling: function() {
